004f8cb0058fdff21b44db6771d8ecfdf084190e
Marc Blank <marc.blank@live.com>
2023-03-23 14:50:22 -0700

    Allow all IDeepSpaceObjectContainer's to be saved as Targets

--- NINA.Sequencer/View/Converter/IsIDeepSkyObjectContainerConverter.cs
+++ NINA.Sequencer/View/Converter/IsIDeepSkyObjectContainerConverter.cs
@@ -1,7 +1,6 @@
 ﻿#region "copyright"
-
 /*
-    Copyright © 2016 - 2023 Stefan Berg <isbeorn86+NINA@googlemail.com> and the N.I.N.A. contributors
+    Copyright © 2016 - 2023 Stefan Berg <isbeorn86+NINA@googlemail.com> and the N.I.N.A. contributors 
 
     This file is part of N.I.N.A. - Nighttime Imaging 'N' Astronomy.
 
@@ -9,23 +8,29 @@
     License, v. 2.0. If a copy of the MPL was not distributed with this
     file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
-
 #endregion "copyright"
-
+using NINA.Core.Enum;
+using NINA.Sequencer;
+using NINA.Sequencer.Container;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
-namespace NINA.Core.Utility.Converters {
 
-    public class DataTypeToStringConverter : IValueConverter {
-
+namespace NINA.View.Sequencer.Converter {
+    public class IsIDeepSkyObjectContainerConverter : IValueConverter {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            return value?.GetType() ?? Binding.DoNothing;
+            if (value is IDeepSkyObjectContainer) return true;
+            return false;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
             throw new NotImplementedException();
         }
     }
-}
+ }
