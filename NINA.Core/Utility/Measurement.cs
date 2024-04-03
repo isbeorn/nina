eb92c3ed925f14fb9a725eaa9c6418ae030d5f6f
Stefan B <isbeorn86@googlemail.com>
2024-04-03 01:41:59 +0000

    Merged in feature/solve_statistics (pull request #1605)
    Feature/solve statistics
    
    * Add sub measurements for centering
    
    * Enrich the CenteringSolver result with measurements for each step
    
    * Merge branch 'develop' into feature/solve_statistics
    
    * Add a new interface method for the explicit return type

---
+++ NINA.Core/Utility/Measurement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NINA.Core.Utility {
+    public class Measurement {
+        private List<Measurement> subMeasurements;
+        public Measurement(string name) {
+            Name = name;
+            StartTime = DateTimeOffset.UtcNow;
+            EndTime = DateTimeOffset.UtcNow;
+            subMeasurements = new List<Measurement>();
+        }
+
+        public Measurement Start() {
+            StartTime = DateTimeOffset.UtcNow;
+            return this;
+        }
+
+        public Measurement Stop() {
+            EndTime = DateTimeOffset.UtcNow;
+            return this;
+        }
+
+        public string Name { get; }
+        public TimeSpan Duration => EndTime - StartTime;
+        public DateTimeOffset StartTime { get; private set; }
+        public DateTimeOffset EndTime { get; private set; }
+        public IReadOnlyCollection<Measurement> SubMeasurements { get => subMeasurements.AsReadOnly(); }
+
+        public Measurement AddSubMeasurement(Measurement measurement) {
+            subMeasurements.Add(measurement);
+            return this;
+        }
+
+        public override string ToString() {
+            return $"{Name} - {Duration}";
+        }
+    }
+}
