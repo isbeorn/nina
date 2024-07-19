53f842da4e240f47c2f5b5eece5fd8b51c0634ac
Stefan Berg <isbeorn86@googlemail.com>
2024-07-19 17:17:05 +0200

    Revert change to skip re-init the sdk on video mode, as it seems to cause issue for the first frames otherwise

--- NINA.Equipment/Equipment/MyCamera/QHYCamera.cs
+++ NINA.Equipment/Equipment/MyCamera/QHYCamera.cs
@@ -759,10 +759,9 @@ namespace NINA.Equipment.Equipment.MyCamera {
 
             try {
                 if (!internalReconnect) {
-                    Sdk.InitSdk();
                     Logger.Info($"QHYCCD: Connecting to {Info.Id}");
                 }
-
+                Sdk.InitSdk();
 
                 /*
                  * Get our selected camera's ID from the SDK
@@ -1053,11 +1052,7 @@ namespace NINA.Equipment.Equipment.MyCamera {
                 }
 
                 Sdk.Close();
-
-                // Do not release the SDK if we are reconnecting to switch between modes. Only need to close and reopen to do that.
-                if (!internalReconnect) {
-                    Sdk.ReleaseSdk();
-                }
+                Sdk.ReleaseSdk();
             } catch (Exception ex) {
                 Logger.Error("QHYCCD: Failed to disconnect", ex);
             }
