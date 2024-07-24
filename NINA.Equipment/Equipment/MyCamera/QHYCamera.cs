9e8da76cf614b2c475fdd5aac17e808c069f3e2c
Stefan Berg <isbeorn86@googlemail.com>
2024-07-24 16:55:11 +0200

    QHY - Prevent spawning a cooler task each time a mode switch happens

--- NINA.Equipment/Equipment/MyCamera/QHYCamera.cs
+++ NINA.Equipment/Equipment/MyCamera/QHYCamera.cs
@@ -939,19 +939,19 @@ namespace NINA.Equipment.Equipment.MyCamera {
                     if (!internalReconnect) {
                         CoolerOn = false;
                         CancelCoolingSync();
+
+                        /*
+                         * Start the thread that operates the TEC
+                         * This thread will operate the TEC in accordance with the user turning the cooler on or off
+                         * and program the camera's TEC to cool to the desired temperature. This thread will operate
+                         * for as long as the camera is connected.
+                         */
+                        Logger.Debug("QHYCCD: Starting CoolerWorker task");
+                        coolerWorkerCts = new CancellationTokenSource();
+                        coolerTask = CoolerWorker(coolerWorkerCts.Token);
                     } else {
                         CoolerOn = tempCoolerOn;
                     }
-
-                    /*
-                     * Start the thread that operates the TEC
-                     * This thread will operate the TEC in accordance with the user turning the cooler on or off
-                     * and program the camera's TEC to cool to the desired temperature. This thread will operate
-                     * for as long as the camera is connected.
-                     */
-                    Logger.Debug("QHYCCD: Starting CoolerWorker task");
-                    coolerWorkerCts = new CancellationTokenSource();
-                    coolerTask = CoolerWorker(coolerWorkerCts.Token);
                 }
 
                 /*
