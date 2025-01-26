9ad135ed6d154dadca1dea6be78a93236861ad02
Marc VBlank <marc.blank@live.com>
2025-01-25 16:21:17 -0800

    Remove SetSwitchValue from commit

--- NINA.Sequencer/SequenceItem/Switch/SetSwitchValue.cs
+++ NINA.Sequencer/SequenceItem/Switch/SetSwitchValue.cs
@@ -71,9 +71,13 @@ namespace NINA.Sequencer.SequenceItem.Switch {
 
         private double value;
 
+        [JsonProperty]
         public double Value {
             get => value;
             set {
+                this.value = value;
+                Validate();
+                RaisePropertyChanged();
             }
         }
 
