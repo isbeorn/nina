1fc1499b6ef54b5837e0619ef581e6c0ae1e0c8b
Marc Blank <marc.blank@live.com>
2025-03-15 20:39:22 -0700

    Revert "Reuse some xaml"
    This reverts commit 4ff1f6697c09f764fe466e9246872aee635806e8.

--- NINA.Sequencer/Logic/SymbolBrokerVM.cs
+++ NINA.Sequencer/Logic/SymbolBrokerVM.cs
@@ -204,7 +204,6 @@ namespace NINA.Sequencer.Logic {
                     DataSource s = list[idx];
                     if (s.source == source) {
                         s.data = value;
-                        s.silent = silent;
                         found = true;
                         break;
                     }
