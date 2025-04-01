2ad88ee274c899cf5cbd523726f82d37632dc8e4
Marc Blank <marc.blank@live.com>
2025-04-01 14:18:29 -0700

    TakeExposure+ working

--- NINA.Sequencer/Serialization/PowerupUpgrader.cs
+++ NINA.Sequencer/Serialization/PowerupUpgrader.cs
@@ -1,7 +1,11 @@
 ﻿using CsvHelper;
 using Microsoft.CodeAnalysis.FlowAnalysis;
+using Nikon;
+using NINA.Core.Model.Equipment;
+using NINA.Equipment.Model;
 using NINA.Sequencer.SequenceItem;
 using NINA.Sequencer.SequenceItem.Expressions;
+using NINA.Sequencer.SequenceItem.Imaging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,8 +21,17 @@ namespace NINA.Sequencer.Serialization {
         private static T CreateNewInstruction<T>(string oldName) {
             var method = sequencerFactory.GetType().GetMethod(nameof(sequencerFactory.GetItem)).MakeGenericMethod(new Type[] { typeof(T) });
             T newObj = (T)method.Invoke(sequencerFactory, null);
+            // For now...
             ((ISequenceItem)newObj).Name += " [SP: " + oldName;
             return newObj;
+        }
+
+        private static string GetExpr (Type t, ISequenceItem item, string name) {
+            PropertyInfo pi = t.GetProperty(name);
+            object expr = pi.GetValue(item);
+            pi = expr.GetType().GetProperty("Expression");
+            return pi.GetValue(expr) as string;
+
         }
 
         public static object UpgradeIntruction(JsonCreationConverter<ISequenceItem> conv, object obj) {
@@ -31,6 +44,18 @@ namespace NINA.Sequencer.Serialization {
             ISequenceItem item = obj as ISequenceItem;
             Type t = item.GetType();
             switch (t.Name) {
+                case "TakeExposure": {
+                        TakeExposure newObj = CreateNewInstruction<TakeExposure>(item.Name);
+                        newObj.ExposureTimeExpression.Definition = GetExpr(t, item, "EExpr");
+                        newObj.GainExpression.Definition = GetExpr(t, item, "GExpr");
+                        newObj.OffsetExpression.Definition = GetExpr(t, item, "OExpr");
+
+                        PropertyInfo pi = t.GetProperty("Binning");
+                        newObj.Binning = (BinningMode)pi.GetValue(item);
+                        pi = t.GetProperty("ImageType");
+                        newObj.ImageType = (string)pi.GetValue(item);
+                        return newObj;
+                    }
                 case "SetConstant": {
                         DefineConstant newObj = CreateNewInstruction<DefineConstant>(item.Name);
                         PropertyInfo pi = t.GetProperty("Definition");
