isbeorn/nina

commit 5fa7e52ba93c5e57bbbb85c5b976de2b4e3ce828

Merged in mjd-keywords (pull request #1680)
Add Modified Julian Date (MJD) FITS keywords and file pattern

* Add Modified Julian Date (MJD) FITS keywords and file pattern

* Ignore RELEASE_NOTES.html

NINA.Core/Model/ImagePattern.cs
@@ -53,6 +53,9 @@ namespace NINA.Core.Model {
             p = new ImagePattern(ImagePatternKeys.TimeUtc, Locale.Loc.Instance["LblTimeUTCFormatDescription"], Locale.Loc.Instance["LblTime"]);
             patterns.Add(p.Key, p);
 
+            p = new ImagePattern(ImagePatternKeys.MJD, Locale.Loc.Instance["LblMJDFormatDescription"], Locale.Loc.Instance["LblTime"]);
+            patterns.Add(p.Key, p);
+
             p = new ImagePattern(ImagePatternKeys.FrameNr, Locale.Loc.Instance["LblFrameNrDescription"], Locale.Loc.Instance["LblImage"]);
             patterns.Add(p.Key, p);
 
@@ -141,9 +144,9 @@ namespace NINA.Core.Model {
             return false;
         }
 
-        public bool Set(string key, double value) {
+        public bool Set(string key, double value, int precision = 2) {
             if (!double.IsNaN(value)) {
-                return this.Set(key, string.Format(CultureInfo.InvariantCulture, "{0:0.00}", value));
+                return this.Set(key, string.Format(CultureInfo.InvariantCulture, value.ToString($"F{precision}")));
             }
             return false;
         }
@@ -201,6 +204,7 @@ namespace NINA.Core.Model {
             p.Set(ImagePatternKeys.DateTime, "2016-01-01_12-00-00");
             p.Set(ImagePatternKeys.Time, "12-00-00");
             p.Set(ImagePatternKeys.TimeUtc, "12-00-00");
+            p.Set(ImagePatternKeys.MJD, "12345.6789");
             p.Set(ImagePatternKeys.FrameNr, "0001");
             p.Set(ImagePatternKeys.ImageType, "LIGHT");
             p.Set(ImagePatternKeys.Binning, "1x1");
@@ -245,6 +249,7 @@ namespace NINA.Core.Model {
         public static readonly string DateTime = "$$DATETIME$$";
         public static readonly string Time = "$$TIME$$";
         public static readonly string TimeUtc = "$$TIMEUTC$$";
+        public static readonly string MJD = "$$MJD$$";
         public static readonly string FrameNr = "$$FRAMENR$$";
         public static readonly string ImageType = "$$IMAGETYPE$$";
         public static readonly string Binning = "$$BINNING$$";

.gitignore
@@ -90,6 +90,7 @@ TestResult.xml
 !**/[Pp]ackages/build/
 
 # Others
+RELEASE_NOTES.html
 ClientBin/
 ~$*
 *~

NINA.Astrometry/DateTimeExtensions.cs
@@ -1,0 +1,46 @@
+﻿#region "copyright"
+/*
+    Copyright © 2016 - 2025 Stefan Berg <isbeorn86+NINA@googlemail.com> and the N.I.N.A. contributors 
+
+    This file is part of N.I.N.A. - Nighttime Imaging 'N' Astronomy.
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+#endregion "copyright"
+
+using System;
+
+namespace NINA.Astrometry {
+    public static class DateTimeExtensions {
+        /// <summary>
+        /// DateTime to Julian Date
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static double ToJD(this DateTime dateTime) {
+            return AstroUtil.GetJulianDate(dateTime);
+        }
+
+        /// <summary>
+        /// DateTime to Modified Julian Date as defined by the SAO in 1957
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static double ToMJD(this DateTime dateTime) {
+            // Modified Julian Date, SAO 1957 definition
+            return AstroUtil.GetJulianDate(dateTime) - 2400000.5;
+        }
+
+        /// <summary>
+        /// DateTime to Modified Julian Date, J2000, as defined by the ESA
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static double ToMJD2000(this DateTime dateTime) {
+            // Modified Julian Date, J2000, ESA
+            return AstroUtil.GetJulianDate(dateTime) - 2451545.0;
+        }
+    }
+}

NINA.Core/Locale/Locale.resx
@@ -7166,6 +7166,9 @@ Additionally, it is essential that the ASCOM driver used also supports this 32-b
   <data name="LblGnssFix2D" xml:space="preserve">
     <value>Current fix is 2D. A 3D fix is required</value>
   </data>
+  <data name="LblMJDFormatDescription" xml:space="preserve">
+    <value>Modified Julian Date</value>
+  </data>
   <data name="LblFlatDeviceOpenWhenDone" xml:space="preserve">
     <value>Open cover when done</value>
     <comment>Open the flat panel cover after the flat wizard has finished.</comment>

NINA.Image/FileFormat/FITS/FITSHeader.cs
@@ -21,7 +21,6 @@ using System.IO;
 using System.Text;
 using NINA.Core.Utility;
 using NINA.Image.ImageData;
-using System.Windows.Input;
 
 namespace NINA.Image.FileFormat.FITS {
     public class FITSHeader {
@@ -516,12 +515,14 @@ namespace NINA.Image.FileFormat.FITS {
             if (metaData.Image.ExposureStart > DateTime.MinValue) {
                 Add("DATE-LOC", metaData.Image.ExposureStart.ToLocalTime(), "Time of observation (local)");
                 Add("DATE-OBS", metaData.Image.ExposureStart.ToUniversalTime(), "Time of observation (UTC)");
+                Add("MJD-OBS", metaData.Image.ExposureStart.ToMJD(), "Modified Julian Date of observation");
             }
 
             if (metaData.Image.ExposureMidPoint > DateTime.MinValue) {
                 // Section 8.4.1 https://fits.gsfc.nasa.gov/standard40/fits_standard40aa-le.pdf
                 // Calendar date of the midpoint of the observation, expressed in the same way as the DATE-OBS keyword.
                 Add("DATE-AVG", metaData.Image.ExposureMidPoint, "Averaged midpoint time (UTC)");
+                Add("MJD-AVG", metaData.Image.ExposureMidPoint.ToMJD(), "Modified Julian Date of averaged midpoint");
             }
 
             /* Camera */

NINA.Image/ImageData/BaseImageData.cs
@@ -12,6 +12,7 @@
 
 #endregion "copyright"
 
+using NINA.Astrometry;
 using NINA.Core.Enum;
 using NINA.Core.Interfaces;
 using NINA.Core.Model;
@@ -238,6 +239,7 @@ namespace NINA.Image.ImageData {
             p.Set(ImagePatternKeys.Time, metadata.Image.ExposureStart.ToLocalTime().ToString("HH-mm-ss"));
             p.Set(ImagePatternKeys.TimeUtc, metadata.Image.ExposureStart.ToUniversalTime().ToString("HH-mm-ss"));
             p.Set(ImagePatternKeys.DateTime, metadata.Image.ExposureStart.ToLocalTime().ToString("yyyy-MM-dd_HH-mm-ss"));
+            p.Set(ImagePatternKeys.MJD, metadata.Image.ExposureStart.ToMJD(), precision: 8);
             p.Set(ImagePatternKeys.FrameNr, metadata.Image.ExposureNumber.ToString("0000"));
             p.Set(ImagePatternKeys.ImageType, metadata.Image.ImageType);
             p.Set(ImagePatternKeys.TargetName, metadata.Target.Name);

commit 6f8f0259fcdc5cba8b96e412e3f2ec9c0eb1a166

Fix double precision culture format issue introduced by 5fa7e52ba93c5e57bbbb85c5b976de2b4e3ce828

NINA.Core/Model/ImagePattern.cs
@@ -146,7 +146,7 @@ namespace NINA.Core.Model {
 
         public bool Set(string key, double value, int precision = 2) {
             if (!double.IsNaN(value)) {
-                return this.Set(key, string.Format(CultureInfo.InvariantCulture, value.ToString($"F{precision}")));
+                return this.Set(key, value.ToString($"F{precision}", CultureInfo.InvariantCulture));
             }
             return false;
         }