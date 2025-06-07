07451e35afce93b39bab1adc50e5b68e7d83d74c
Stefan Berg <isbeorn86@googlemail.com>
2025-06-07 20:54:48 +0200

    Adding a native preload for sofa and novas

--- NINA.Test/Usings.cs
+++ NINA.Test/Usings.cs
@@ -1,1 +1,15 @@
 global using NUnit.Framework;
+using NINA.Core.Utility;
+
+[SetUpFixture]
+public class Init {
+    [OneTimeSetUp]
+    public void LoadAllNativeDlls() {
+        Console.WriteLine("Preloading Native DLLs.");
+        Console.WriteLine(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "External", "x64", Path.Combine("SOFA", "SOFAlib.dll")));
+        DllLoader.LoadDll(Path.Combine("SOFA", "SOFAlib.dll"));
+        Console.WriteLine(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "External", "x64", Path.Combine("NOVAS", "NOVAS31lib.dll")));
+        DllLoader.LoadDll(Path.Combine("NOVAS", "NOVAS31lib.dll"));
+        Console.WriteLine("Native DLLs preloaded.");
+    }
+}
