b79b6ecffda8665a090543664a35ec08432b66c4
Stefan Berg <isbeorn86@googlemail.com>
2025-06-24 03:07:05 +0200

    Replace WebRequest.Create with HttpClient as WebRequest is deprecated

--- NINA.Core/Utility/Http/HttpUploadFile.cs
+++ NINA.Core/Utility/Http/HttpUploadFile.cs
@@ -16,6 +16,7 @@ using System;
 using System.Collections.Specialized;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -36,63 +37,38 @@ namespace NINA.Core.Utility.Http {
 
         public override async Task<string> Request(CancellationToken ct, IProgress<int> progress = null) {
             string result = string.Empty;
-            string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
-            byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
 
-            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(Url);
-            wr.UserAgent = CoreUtil.UserAgent;
-            wr.ContentType = "multipart/form-data; boundary=" + boundary;
-            wr.Method = "POST";
-            wr.Timeout = (int)TimeSpan.FromSeconds(300).TotalMilliseconds;
-            //wr.KeepAlive = true;
-            //wr.Credentials = System.Net.CredentialCache.DefaultCredentials;
+            try {
+                using var httpClient = new HttpClient {
+                    Timeout = TimeSpan.FromSeconds(300)
+                };
 
-            Stream rs = wr.GetRequestStream();
+                httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(CoreUtil.UserAgent);
 
-            string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
-            foreach (string key in NameValueCollection.Keys) {
-                rs.Write(boundarybytes, 0, boundarybytes.Length);
-                string formitem = string.Format(formdataTemplate, key, NameValueCollection[key]);
-                byte[] formitembytes = System.Text.Encoding.UTF8.GetBytes(formitem);
-                rs.Write(formitembytes, 0, formitembytes.Length);
-            }
-            rs.Write(boundarybytes, 0, boundarybytes.Length);
+                using var form = new MultipartFormDataContent();
 
-            string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
-            string header = string.Format(headerTemplate, ParamName, File.Name, ContentType);
-            byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
-            rs.Write(headerbytes, 0, headerbytes.Length);
-
-            // FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
-            byte[] buffer = new byte[4096];
-            int bytesRead = 0;
-            while ((bytesRead = File.Read(buffer, 0, buffer.Length)) != 0) {
-                rs.Write(buffer, 0, bytesRead);
-                ct.ThrowIfCancellationRequested();
-            }
-            File.Close();
-
-            byte[] trailer = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
-            rs.Write(trailer, 0, trailer.Length);
-            ct.ThrowIfCancellationRequested();
-            WebResponse wresp = null;
-            using (ct.Register(() => wr.Abort(), useSynchronizationContext: false)) {
-                try {
-                    wresp = await wr.GetResponseAsync();
-                    using (var streamReader = new StreamReader(wresp.GetResponseStream())) {
-                        result = streamReader.ReadToEnd();
-                    }
-                } catch (Exception ex) {
-                    ct.ThrowIfCancellationRequested();
-                    Logger.Error(ex);
-                    Notification.Notification.ShowError(string.Format("Unable to connect to {0}", Url));
-
-                    wresp?.Close();
-                    wresp = null;
-                } finally {
-                    wr = null;
+                // Add all key-value form fields
+                foreach (string key in NameValueCollection.Keys) {
+                    string value = NameValueCollection[key];
+                    form.Add(new StringContent(value), key);
                 }
+
+                var fileContent = new StreamContent(File);
+                fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(ContentType);
+
+                form.Add(fileContent, ParamName, Path.GetFileName(File.Name));
+
+                using var response = await httpClient.PostAsync(Url, form, ct);
+                response.EnsureSuccessStatusCode();
+
+                result = await response.Content.ReadAsStringAsync(ct);
+            } catch (OperationCanceledException) {
+                ct.ThrowIfCancellationRequested();
+            } catch (Exception ex) {
+                Logger.Error(ex);
+                Notification.Notification.ShowError($"Unable to connect to {Url}");
             }
+
             return result;
         }
     }
