fcd308171d64891e3c7499c42700727b4c5fd393
Stefan Berg <isbeorn86@googlemail.com>
2025-10-11 06:12:31 +0200

    cleanup

--- NINA/View/Sequencer/AdvancedSequencer/AdvancedSequencerView.xaml.cs
+++ NINA/View/Sequencer/AdvancedSequencer/AdvancedSequencerView.xaml.cs
@@ -12,14 +12,20 @@
 
 #endregion "copyright"
 
-using NINA.Sequencer.Logic;
-using NINA.ViewModel.Sequencer;
-using NJsonSchema.Annotations;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace NINA.View.Sequencer.AdvancedSequencer {
 
