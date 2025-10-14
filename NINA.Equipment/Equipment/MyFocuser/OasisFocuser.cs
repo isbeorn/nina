3a7e229a4ad4fe195ec8c9c01422dda33db5bae7
Nico Trost <nico.trost@amd.com>
2025-10-14 09:43:13 +0200

    Added Oasis focuser

---
+++ NINA.Equipment/Equipment/MyFocuser/OasisFocuser.cs
@@ -0,0 +1,303 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using NINA.Core.Locale;
+using NINA.Core.MyMessageBox;
+using NINA.Core.Utility;
+using NINA.Core.Utility.Notification;
+using NINA.Equipment.Interfaces;
+using NINA.Profile.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using static Astroasis.AstroasisSDK.AOFocus;
+
+namespace NINA.Equipment.Equipment.MyFocuser {
+    public partial class OasisFocuser : BaseINPC, IFocuser {
+        private readonly IProfileService profileService;
+        private int id;
+        private static TimeSpan SameFocuserPositionTimeout = TimeSpan.FromMinutes(1);
+
+        public OasisFocuser(int id, IProfileService profileService) {
+            this.profileService = profileService;
+
+            this.id = id;
+
+            FocuserGetProductModel(id, out var model);
+
+            focuserAlias = GetAlias();
+
+            Logger.Debug($"Oasis: Focuser ID/Alias: {focuserAlias}");
+
+            Name = model;
+
+            SetId();
+        }
+        public bool IsMoving { 
+            get {
+                var err = FocuserGetStatus(id, out var status);
+                if (err == AOReturn.AO_SUCCESS) {
+                    return status.moving == 1;
+                } else {
+                    if (err == AOReturn.AO_ERROR_COMMUNICATION) {
+                        Logger.Error($"Oasis communication error to get moving state {err}");
+                        DisconnectOnRemovedError();
+                    } else {
+                        Logger.Error($"Oasis communication error to get moving state {err}");
+                    }
+                    return false;
+                }
+            }
+        }
+
+        public int MaxIncrement => MaxStep;
+
+        public int MaxStep {
+            get {
+                var err = FocuserGetConfig(id, out var config);
+                if (err == AOReturn.AO_SUCCESS) {
+                    return config.maxStep;
+                } else {
+                    Logger.Error($"Oasis communication error to get MaxStep {err}");
+                    return -1;
+                }
+            }
+        }
+
+        public int Position {
+            get {
+                var err = FocuserGetStatus(id, out var status);
+                if (err == AOReturn.AO_SUCCESS) {
+                    return status.position;
+                } else {
+                    if (err == AOReturn.AO_ERROR_COMMUNICATION) {
+                        Logger.Error($"Oasis communication error to get Position state {err}");
+                        DisconnectOnRemovedError();
+                    } else {
+                        Logger.Error($"Oasis Communication error to get Position {err}");
+                    }
+                    return -1;
+                }
+            }
+        }
+
+        private void DisconnectOnRemovedError() {
+            try {
+                Notification.ShowWarning(Loc.Instance["LblFocuserConnectionLost"]);
+                Logger.Error($"Oasis device was removed");
+                Disconnect();
+            } catch (Exception ex) {
+                Logger.Error(ex);
+            }
+        }
+
+        public double StepSize => -1;
+
+        public double Temperature {
+            get {
+                var err = FocuserGetStatus(id, out var status);
+                if (err == AOReturn.AO_SUCCESS) {
+                    if (status.temperatureDetection == 0) {
+                        // Ambient probe not connected, display internal temperature
+                        return status.temperatureInt / 100.0;
+                    } else {
+                        return status.temperatureExt / 100.0;
+                    }
+                } else {
+                    if (err == AOReturn.AO_ERROR_COMMUNICATION) {
+                        Logger.Error($"Oasis communication error to get Temperature {err}");
+                        DisconnectOnRemovedError();
+                    } else {
+                        Logger.Error($"Oasis communication error to get Temperature {err}");
+                    }
+                    return double.NaN;
+                }
+            }
+        }
+
+        public string Id { get; private set; }
+        private void SetId() {
+            Id = $"{Category}_{Name}_{FocuserAlias}";
+        }
+
+        private string focuserAlias;
+
+        public string FocuserAlias {
+            get {
+                return focuserAlias;
+            }
+
+            set {
+                Logger.Debug($"Oasis: Setting Focuser ID/Alias to: {value}");
+
+                FocuserSetFriendlyName(id, value);
+                focuserAlias = GetAlias();
+
+                FocuserGetFriendlyName(id, out var name);
+                Name = new string(name);
+                SetId();
+
+                Logger.Info($"Oasis: Focuser ID/Alias set to: {focuserAlias}");
+
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(Name));
+                RaisePropertyChanged(nameof(DisplayName));
+                RaisePropertyChanged(nameof(Id));
+                profileService.ActiveProfile.FocuserSettings.Id = Id;
+            }
+        }
+
+        private string GetAlias() {
+            FocuserGetFriendlyName(id, out var name);
+            var strName = new string(name);
+
+            if (strName.Contains('(') && strName.Contains(')') && strName.EndsWith(")")) {
+                var openparen = strName.IndexOf('(');
+                var closeparen = strName.LastIndexOf(')');
+                var alias = closeparen - openparen;
+                return strName.Substring(openparen + 1, alias - 1);
+            } else {
+                return string.Empty;
+            }
+        }
+
+        public string Name { get; private set; }
+        public string DisplayName => Name;
+
+        public string Category => "Oasis";
+
+        [ObservableProperty]
+        private bool connected = false;
+
+        [ObservableProperty]
+        private bool reversed = false;
+
+        partial void OnReversedChanged(bool value)
+        {
+            AOFocuserConfig config = new AOFocuserConfig();
+            config.mask = (uint)AOConfig.MASK_REVERSE_DIRECTION;
+            config.reverseDirection = value ? 1 : 0;
+            _ = FocuserSetConfig(id, ref config);
+        }
+
+        [ObservableProperty]
+        private int targetMaxStep;
+
+        partial void OnTargetMaxStepChanged(int value) {
+            AOFocuserConfig config = new AOFocuserConfig();
+            config.mask = (uint)AOConfig.MASK_MAX_STEP;
+            config.maxStep = value;
+            _ = FocuserSetConfig(id, ref config);
+
+            RaisePropertyChanged(nameof(MaxStep));
+            RaisePropertyChanged(nameof(MaxIncrement));
+        }
+
+        [RelayCommand]
+        public void ResetPosition() {
+            if (MyMessageBox.Show(Loc.Instance["LblZwoResetZeroPositionPrompt"], "", System.Windows.MessageBoxButton.YesNo, System.Windows.MessageBoxResult.No) == System.Windows.MessageBoxResult.Yes) {
+                if(Position > 0) {
+                    FocuserSetZeroPosition(id);
+                    RaisePropertyChanged(nameof(Position));
+                }
+            }
+        }
+
+        public string Description => "Native driver for Oasis focusers";
+
+        public string DriverInfo { get; private set; } = string.Empty;
+
+        public string DriverVersion {
+            get {
+                _ = FocuserGetSDKVersion(out var version);
+                return new string(version);
+            }
+        }
+
+        public Task<bool> Connect(CancellationToken token) {
+            return Task.Run(() => {
+                if (FocuserOpen(id) == AOReturn.AO_SUCCESS) {
+                    DriverInfo = $"SDK: {DriverVersion}; FW: {GetFwVersionString()}";
+
+                    FocuserGetConfig(id, out var config);
+                    Reversed = config.reverseDirection == 1;
+
+                    TargetMaxStep = config.maxStep;
+
+                    Connected = true;
+                    return true;
+                } else {
+                    Logger.Error("Failed to connect to Oasis focuser");
+                    return false;
+                }
+            });
+        }
+
+        private string GetFwVersionString() {
+            AOFocuserVersion version = new AOFocuserVersion();
+            _ = FocuserGetVersion(id, out version);
+            return version.firmware.ToString();
+        }
+
+        public void Disconnect() {
+            _ = FocuserClose(id);
+            this.Connected = false;
+        }
+
+        public void Halt() {
+            FocuserStopMove(id);
+        }
+
+        public async Task Move(int position, CancellationToken ct, int waitInMs = 1000) {
+
+            var lastPosition = int.MinValue;
+            int samePositionCount = 0;
+            var lastMovementTime = DateTime.Now;
+            while (position != Position && !ct.IsCancellationRequested) {
+                FocuserMoveTo(id, position);
+
+                if (lastPosition == Position) {
+                    ++samePositionCount;
+                    var samePositionTime = DateTime.Now - lastMovementTime;
+                    if (samePositionTime >= SameFocuserPositionTimeout) {
+                        throw new Exception($"Focuser stuck at position {lastPosition} beyond {SameFocuserPositionTimeout} timeout");
+                    }
+
+                    // Make sure we wait in between Move requests when no progress is being made
+                    // to avoid spamming the driver and spiking the CPU
+                    await CoreUtil.Wait(TimeSpan.FromSeconds(1), ct);
+                } else {
+                    lastMovementTime = DateTime.Now;
+                }
+
+                FocuserGetStatus(id, out var status);
+                lastPosition = status.position;
+            }
+        }
+
+
+        #region Unsupported
+        public bool TempCompAvailable => false;
+        public bool TempComp { get; set; }
+        public bool HasSetupDialog => false;
+        public IList<string> SupportedActions => new List<string>();
+        public void SetupDialog() {
+        }
+
+        public string Action(string actionName, string actionParameters) {
+            throw new NotImplementedException();
+        }
+        public void SendCommandBlind(string command, bool raw = true) {
+            throw new NotImplementedException();
+        }
+
+        public bool SendCommandBool(string command, bool raw = true) {
+            throw new NotImplementedException();
+        }
+
+        public string SendCommandString(string command, bool raw = true) {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ NINA.Equipment/SDK/FocuserSDKs/OasisSDK/AOFocus.cs
@@ -0,0 +1,309 @@
+﻿using NINA.Core.Utility;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Astroasis.AstroasisSDK {
+    public class AOFocus {
+        private const string DLLNAME = "OasisFocuser.dll";
+
+        static AOFocus() {
+            DllLoader.LoadDll(Path.Combine("Oasis", DLLNAME));
+        }
+
+        public const int AO_FOCUSER_MAX_NUM = 32;        /* Maximum number of focusers which can be connected to the system */
+        public const int AO_FOCUSER_VERSION_LEN = 32;    /* Length of version string */
+        public const int AO_FOCUSER_NAME_LEN = 32;       /* Length of name string */
+
+        public const int AO_LOG_LEVEL_QUIET = 0;    /* No log */
+        public const int AO_LOG_LEVEL_ERROR = 1;    /* Error log */
+        public const int AO_LOG_LEVEL_INFO = 2;     /* Info and error log */
+        public const int AO_LOG_LEVEL_DEBUG = 3;    /* Debug, info and error log */
+
+        public const int TEMPERATURE_INVALID = unchecked((int)0x80000000);  /* Invalid temperature value */
+
+        public enum AOReturn : int {
+            AO_SUCCESS = 0,                     /* Success */
+            AO_ERROR_INVALID_ID,                /* Device ID is invalid */
+            AO_ERROR_INVALID_PARAMETER,         /* One or more parameters are invalid */
+            AO_ERROR_INVALID_STATE,             /* Device is not in correct state for specific API call */
+            AO_ERROR_BUFFER_TOO_SMALL,          /* Size of buffer is too small */
+            AO_ERROR_COMMUNICATION,             /* Data communication error such as device has been removed from USB port */
+            AO_ERROR_TIMEOUT,                   /* Timeout occured */
+            AO_ERROR_BUSY,                      /* Device is being used by another application */
+            AO_ERROR_NULL_POINTER,              /* Caller passes null-pointer parameter which is not expected */
+            AO_ERROR_OUT_OF_RESOURCE,           /* Out of resouce such as lack of memory */
+            AO_ERROR_NOT_IMPLEMENTED,           /* The interface is not currently supported */
+            AO_ERROR_FAULT,                     /* Significant fault which means the device may not work correctly and hard to recovery it */
+            AO_ERROR_INVALID_SIZE,              /* Size is invalid */
+            AO_ERROR_INVALID_VERSION,           /* Version is invalid */
+            AO_ERROR_UNKNOWN = 0x40,            /* Any other errors */
+        }
+
+        public enum AOConfig : uint {
+            MASK_MAX_STEP = 0x00000001,             /* Used to set maxStep field */
+            MASK_BACKLASH = 0x00000002,             /* Used to set backlash field */
+            MASK_BACKLASH_DIRECTION = 0x00000004,   /* Used to set backlashDirection field */
+            MASK_REVERSE_DIRECTION = 0x00000008,    /* Used to set reverseDirection field */
+            MASK_SPEED = 0x00000010,                /* Used to set speed field */
+            MASK_BEEP_ON_MOVE = 0x00000020,         /* Used to set beepOnMove field */
+            MASK_BEEP_ON_STARTUP = 0x00000040,      /* Used to set beepOnStartup field */
+            MASK_BLUETOOTH = 0x00000080,            /* Used to set bluetoothOn field */
+            MASK_STALL_DETECTION = 0x00000100,      /* Used to set stallDetection field */
+            MASK_HEATING_TEMPERATURE = 0x00000200,  /* Used to set heatingTemperature field */
+            MASK_HEATING_ON = 0x00000400,           /* Used to set heatingOn field */
+            MASK_USB_POWER_CAPACITY = 0x00000800,   /* Used to set usbPowerCapacity field */
+            MASK_ALL = 0xFFFFFFFF                   /* Used to set all fields */
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct AOFocuserVersion {
+            public uint protocal;          /* Version of protocal over USB and Bluetooth communication */
+            public uint hardware;          /* Focuser hardware version */
+            public uint firmware;          /* Focuser firmware version */
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 24)]
+            public string built;           /* Null-terminated string which indicates firmware building time */
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct AOFocuserConfig {
+            public uint mask;                      /* Used by AOFocuserSetConfig() to indicates which field wants to be set */
+            public int maxStep;                    /* Maximum step or position */
+            public int backlash;                   /* Backlash value */
+            public int backlashDirection;          /* Backlash direction. 0 - IN, others - OUT */
+            public int reverseDirection;           /* 0 - Not reverse motor moving direction, others - Reverse motor moving direction */
+            public int speed;                      /* Reserved for motor speed setting. Should always be zero for now */
+            public int beepOnMove;                 /* 0 - Turn off beep for move, others - Turn on beep for move */
+            public int beepOnStartup;              /* 0 - Turn off beep for device startup, others - Turn on beep for device startup */
+            public int bluetoothOn;                /* 0 - Turn off Bluetooth, others - Turn on Bluetooth */
+            public int stallDetection;             /* 0 - Turn off motor stall detection, others - Turn on motor stall detection */
+            public int heatingTemperature;         /* Target heating temperature in 0.01 degree unit */
+            public int heatingOn;                  /* 0 - Turn off heating, others - Turn on heating */
+            public int usbPowerCapacity;           /* 0 - 500mA, 1 - 1000mA or higher */
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct AOFocuserStatus {
+            public int temperatureInt;             /* Internal (on board) temperature in 0.01 degree unit */
+            public int temperatureExt;             /* External (ambient) temperature in 0.01 degree unit */
+            public int temperatureDetection;       /* 0 - ambient temperature probe is not inserted, others - ambient temperature probe is inserted */
+            public int position;                   /* Current motor position */
+            public int moving;                     /* 0 - motor is not moving, others - Motor is moving */
+            public int stallDetection;             /* 0 - motor stall is not detected, others - motor stall detected */
+            public int heatingOn;                  /* 0 - heating is disabled, others - heating is enabled */
+            public int heatingPower;               /* Current heating power in 1% unit */
+            public int dcPower;                    /* Current DC power in 0.1V unit */
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 20)]
+            public int[] reserved;
+        };
+
+        [SecurityCritical]
+        public static AOReturn FocuserScan(out int number, [Out] int[] ids) {
+            return AOFocuserScan(out number, ids);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserOpen(int id) {
+            return AOFocuserOpen(id);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserClose(int id) {
+            return AOFocuserClose(id);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserGetProductModel(int id, out string model) {
+            byte[] buf = new byte[256];
+            var err = AOFocuserGetProductModel(id, buf);
+            model = Encoding.ASCII.GetString(buf).TrimEnd('\0');
+            return err;
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserGetVersion(int id, out AOFocuserVersion version) {
+            return AOFocuserGetVersion(id, out version);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserGetSerialNumber(int id, out string sn) {
+            byte[] buf = new byte[256];
+            var err = AOFocuserGetSerialNumber(id, buf);
+            sn = Encoding.ASCII.GetString(buf).TrimEnd('\0');
+            return err;
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserGetFriendlyName(int id, out string name) {
+            byte[] buf = new byte[256];
+            var err = AOFocuserGetFriendlyName(id, buf);
+            name = Encoding.ASCII.GetString(buf).TrimEnd('\0');
+            return err;
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserSetFriendlyName(int id, string name) {
+            return AOFocuserSetFriendlyName(id, name);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserGetBluetoothName(int id, out string name) {
+            byte[] buf = new byte[256];
+            var err = AOFocuserGetBluetoothName(id, buf);
+            name = Encoding.ASCII.GetString(buf).TrimEnd('\0');
+            return err;
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserSetBluetoothName(int id, string name) {
+            return AOFocuserSetBluetoothName(id, name);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserGetConfig(int id, out AOFocuserConfig config) {
+            return AOFocuserGetConfig(id, out config);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserSetConfig(int id, ref AOFocuserConfig config) {
+            return AOFocuserSetConfig(id, ref config);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserGetStatus(int id, out AOFocuserStatus status) {
+            return AOFocuserGetStatus(id, out status);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserFactoryReset(int id) {
+            return AOFocuserFactoryReset(id);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserSetZeroPosition(int id) {
+            return AOFocuserSetZeroPosition(id);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserSyncPosition(int id, int position) {
+            return AOFocuserSyncPosition(id, position);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserMove(int id, int step) {
+            return AOFocuserMove(id, step);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserMoveTo(int id, int position) {
+            return AOFocuserMoveTo(id, position);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserStopMove(int id) {
+            return AOFocuserStopMove(id);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserClearStall(int id) {
+            return AOFocuserClearStall(id);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserUpgrade(int id) {
+            return AOFocuserUpgrade(id);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserFirmwareUpgrade(int id, in byte[] data, int len) {
+            return AOFocuserFirmwareUpgrade(id, data, len);
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserGetSDKVersion(out string version) {
+            byte[] buf = new byte[256];
+            var err = AOFocuserGetSDKVersion(buf);
+            version = Encoding.ASCII.GetString(buf).TrimEnd('\0');
+            return err;
+        }
+
+        [SecurityCritical]
+        public static AOReturn FocuserSetLogLevel(int level) {
+            return AOFocuserSetLogLevel(level);
+        }
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserScan", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserScan(out int number, [Out] int[] ids);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserOpen", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserOpen(int id);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserClose", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserClose(int id);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserGetProductModel", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserGetProductModel(int id, [Out] byte[] model);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserGetVersion", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserGetVersion(int id, out AOFocuserVersion version);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserGetSerialNumber", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserGetSerialNumber(int id, [Out] byte[] sn);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserGetFriendlyName", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserGetFriendlyName(int id, [Out] byte[] name);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserSetFriendlyName", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserSetFriendlyName(int id, string name);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserGetBluetoothName", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserGetBluetoothName(int id, [Out] byte[] name);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserSetBluetoothName", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserSetBluetoothName(int id, string name);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserGetConfig", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserGetConfig(int id, out AOFocuserConfig config);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserSetConfig", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserSetConfig(int id, ref AOFocuserConfig config);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserGetStatus", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserGetStatus(int id, out AOFocuserStatus status);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserFactoryReset", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserFactoryReset(int id);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserSetZeroPosition", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserSetZeroPosition(int id);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserSyncPosition", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserSyncPosition(int id, int position);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserMove", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserMove(int id, int step);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserMoveTo", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserMoveTo(int id, int position);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserStopMove", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserStopMove(int id);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserClearStall", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserClearStall(int id);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserUpgrade", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserUpgrade(int id);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserFirmwareUpgrade", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserFirmwareUpgrade(int id, byte[] data, int len);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserGetSDKVersion", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserGetSDKVersion([Out] byte[] version);
+
+        [DllImport(DLLNAME, EntryPoint = "AOFocuserSetLogLevel", CallingConvention = CallingConvention.Cdecl)]
+        private static extern AOReturn AOFocuserSetLogLevel(int level);
+    }
+}
