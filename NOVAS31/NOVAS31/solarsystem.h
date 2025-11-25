f2636937ab27855907bcdf1c09fa9ec32d972447
Nico <nitro.mail@web.de>
2025-11-25 07:55:26 +0100

    NOVAS

--- NOVAS31/NOVAS31/solarsystem.h
+++ NOVAS31/NOVAS31/solarsystem.h
@@ -15,7 +15,15 @@
    #define _SOLSYS_
 
 
-#define EXPORT __declspec(dllexport)
+#ifndef EXPORT
+# if defined(_WIN32) || defined(__CYGWIN__)
+#  define EXPORT __declspec(dllexport)
+# elif defined(__GNUC__)
+#  define EXPORT __attribute__((visibility("default")))
+# else
+#  define EXPORT
+# endif
+#endif
 
 /*
    Function prototypes
