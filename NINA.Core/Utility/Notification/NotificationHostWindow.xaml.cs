f03888e036c5e1f7e617b3c2554b27390743b986
Stefan Berg <isbeorn86@googlemail.com>
2025-12-02 10:08:01 +0100

    React on display settings changes

--- NINA.Core/Utility/Notification/NotificationHostWindow.xaml.cs
+++ NINA.Core/Utility/Notification/NotificationHostWindow.xaml.cs
@@ -1,7 +1,7 @@
 #region "copyright"
 
 /*
-    Copyright © 2016 - 2024 Stefan Berg <isbeorn86+NINA@googlemail.com> and the N.I.N.A. contributors
+    Copyright © 2016 - 2025 Stefan Berg <isbeorn86+NINA@googlemail.com> and the N.I.N.A. contributors
 
     This file is part of N.I.N.A. - Nighttime Imaging 'N' Astronomy.
 
@@ -12,6 +12,8 @@
 
 #endregion "copyright"
 
+using Microsoft.Win32;
+using System;
 using System.Windows;
 
 namespace NINA.Core.Utility.Notification {
@@ -20,19 +22,52 @@ namespace NINA.Core.Utility.Notification {
         public NotificationHostWindow() {
             InitializeComponent();
 
-            Loaded += (_, __) => Reposition();
+            Loaded += OnLoaded;
             SizeChanged += (_, __) => Reposition();
+            IsVisibleChanged += (_, __) => {
+                if (IsVisible) {
+                    Reposition();
+                }
+            };
+            Activated += (_, __) => Reposition();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e) {
+            Reposition();
+
+            // Subscribe to display changes
+            SystemEvents.DisplaySettingsChanged += OnDisplaySettingsChanged;
+        }
+
+        private void OnDisplaySettingsChanged(object sender, EventArgs e) {
+            // This event is not on the UI thread, so marshal back
+            Dispatcher.BeginInvoke(new Action(Reposition));
+        }
+
+        protected override void OnClosed(EventArgs e) {
+            base.OnClosed(e);
+            SystemEvents.DisplaySettingsChanged -= OnDisplaySettingsChanged;
         }
 
         private void Reposition() {
+            // Always use primary screen work area 
             var workArea = SystemParameters.WorkArea;
+
+            // In case ActualWidth/Height are 0 (not rendered yet), let WPF arrange once
+            if (double.IsNaN(Width) || Width == 0 || double.IsNaN(Height) || Height == 0) {
+                UpdateLayout();
+            }
+
             Left = workArea.Right - ActualWidth;
             Top = workArea.Bottom - ActualHeight;
         }
 
         public void ShowIfNeeded() {
             if (!IsVisible) {
+                Reposition();
                 Show();
+            } else {
+                Reposition();
             }
         }
 
