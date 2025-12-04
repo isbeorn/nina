7130737d1ea9a0e4ff0152968fe94b6ce806e628
Marc Blank <marc.blank@live.com>
2025-12-03 17:36:58 -0800

    Show default string properly when loading default values from sequences

--- NINA.Sequencer.Generators/ExpressionGenerator.cs
+++ NINA.Sequencer.Generators/ExpressionGenerator.cs
@@ -267,7 +267,7 @@ namespace NINA.Sequencer.Generators {
                     propertiesSource += $@";
                         }}
             set {{
-                {propNameExpression}.Definition = Convert.ToString(value, CultureInfo.InvariantCulture);
+                {propNameExpression}.Definition = (value == {propNameExpression}.Default && {propNameExpression}.DefaultString != null) ? String.Empty : Convert.ToString(value, CultureInfo.InvariantCulture);
             }}
         }}
         [JsonProperty (Order = 1)]
