7458c4303a85cf2d956eff5496f0464db361b424
Marc Blank <marc.blank@live.com>
2025-12-10 17:24:07 -0800

    Revise and add test for TakeSubframeExposure

--- NINA.Sequencer/Logic/Expression.cs
+++ NINA.Sequencer/Logic/Expression.cs
@@ -641,9 +641,7 @@ namespace NINA.Sequencer.Logic {
                         try {
                             Value = Convert.ToDouble(eval, CultureInfo.InvariantCulture);
                             // Validate numeric values
-                            if (Validator != null) {
-                                Validator(this);
-                            }
+                            CheckRange(Value);
                         } catch (Exception) {
                             string str = eval as string;
                             if (STRING_VALUES_ALLOWED) {
