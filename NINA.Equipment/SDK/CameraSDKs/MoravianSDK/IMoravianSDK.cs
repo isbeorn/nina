e0e97b4ed530487dbfb1c3003d6d9b9b2769bec0
Stefan Berg <isbeorn86@googlemail.com>
2026-01-06 00:14:27 +0100

    Moravian native driver

---
+++ NINA.Equipment/SDK/CameraSDKs/MoravianSDK/IMoravianSDK.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoravianCameraSDK {
+
+    public interface IMoravianSDK {
+        UIntPtr Initialize(uint cameraId);
+        void Release(UIntPtr handle);
+        void RegisterNotifyHWND(UIntPtr handle, IntPtr hwnd);
+        bool GetBooleanParameter(UIntPtr handle, MoravianBooleanParameter index, out bool value);
+        bool GetIntegerParameter(UIntPtr handle, MoravianIntegerParameter index, out int value);
+        bool GetStringParameter(UIntPtr handle, MoravianStringParameter index, int maxLen, StringBuilder sb);
+        bool GetValue(UIntPtr handle, MoravianValueParameter index, out float value);
+        bool EnumerateReadModes(UIntPtr handle, int index, int maxLen, StringBuilder sb);
+        bool EnumerateFilters(UIntPtr handle, uint index, int maxLen, StringBuilder sb, out uint color);
+        bool EnumerateFilters2(UIntPtr handle, uint index, int maxLen, StringBuilder sb, out uint color, out int offset);
+        bool SetReadMode(UIntPtr handle, int mode);
+        bool SetBinning(UIntPtr handle, uint x, uint y);
+        void SetGain(UIntPtr handle, uint gain);
+        bool SetFilter(UIntPtr handle, uint index);
+        void SetTemperature(UIntPtr handle, float temperature);
+        void SetTemperatureRamp(UIntPtr handle, float ramp);
+        bool SetFan(UIntPtr handle, byte speed);
+        bool SetWindowHeating(UIntPtr handle, bool on);
+        bool SetPreflash(UIntPtr handle, double preflashTime, uint clearNum);
+        bool MoveTelescope(UIntPtr handle, short raDurationMs, short decDurationMs);
+        bool MoveInProgress(UIntPtr handle, out bool moving);
+        void GetLastErrorString(UIntPtr handle, int maxLen, StringBuilder sb);
+        bool OpenShutter(UIntPtr handle);
+        bool CloseShutter(UIntPtr handle);
+    }
+
+    public interface IMoravianCameraTimingExposure {
+        bool StartExposure(UIntPtr handle, double expTime, bool useShutter, int x, int y, int w, int d);
+        bool StartExposureTrigger(UIntPtr handle, double expTime, bool useShutter, int x, int y, int w, int d);
+        bool AbortExposure(UIntPtr handle, bool downloadFlag);
+        bool ImageReady(UIntPtr handle, out bool ready);
+        bool ReadImage(UIntPtr handle, uint bufferLen, ushort[] buffer);
+        bool ReadImageExposure(UIntPtr handle, uint bufferLen, ushort[] buffer);
+
+    }
+
+    public interface IMoravianComputerTimingExposure {
+        bool BeginExposure(UIntPtr handle, bool useShutter);
+        bool EndExposure(UIntPtr handle, bool useShutter, bool abortData);
+        bool GetImage(UIntPtr handle, int x, int y, int w, int d, uint bufferLen, ushort[] buffer);
+        bool GetImage8b(UIntPtr handle, int x, int y, int w, int d, uint bufferLen, ushort[] buffer);
+        bool GetImage16b(UIntPtr handle, int x, int y, int w, int d, uint bufferLen, ushort[] buffer);
+        bool GetImageExposure(UIntPtr handle, double expTime, bool useShutter, int x, int y, int w, int d, uint bufferLen, ushort[] buffer);
+        bool GetImageExposure8b(UIntPtr handle, double expTime, bool useShutter, int x, int y, int w, int d, uint bufferLen, ushort[] buffer);
+        bool GetImageExposure16b(UIntPtr handle, double expTime, bool useShutter, int x, int y, int w, int d, uint bufferLen, ushort[] buffer);
+    }
+
+    public interface IMoravianGPS {
+        bool GetImageTimeStamp(UIntPtr handle, out int year, out int month, out int day, out int hour, out int minute, out double second);
+        bool GetGPSData(UIntPtr handle,
+            out double lat, out double lon, out double msl,
+            out int year, out int month, out int day, out int hour, out int minute, out double second,
+            out uint satellites, out bool fix);
+    }
+
+    public interface IMoravianConfigurable {
+        void Configure(UIntPtr handle, IntPtr parentHwnd);
+    }
+
+    public interface IMoravianSensorClearable {
+        bool ClearSensor(UIntPtr handle);
+    }
+
+    public enum MoravianBooleanParameter : uint {
+        gbpConnected = 0,
+        gbpSubFrame = 1,
+        gbpReadModes = 2,
+        gbpShutter = 3,
+        gbpCooler = 4,
+        gbpFan = 5,
+        gbpFilters = 6,
+        gbpGuide = 7,
+        gbpWindowHeating = 8,
+        gbpPreflash = 9,
+        gbpAsymmetricBinning = 10,
+        gbpMicrometerFilterOffsets = 11,
+        gbpPowerUtilization = 12,
+        gbpGain = 13,
+        gbpElectronicShutter = 14,
+        gbpGPS = 16,
+        gbpContinuousExposures = 17,
+        gbpTrigger = 18,
+        gbpConfigured = 127,
+        gbpRGB = 128,
+        gbpCMY = 129,
+        gbpCMYG = 130,
+        gbpDebayerXOdd = 131,
+        gbpDebayerYOdd = 132,
+        gbpInterlaced = 256
+    }
+
+    public enum MoravianIntegerParameter : uint {
+        gipCameraId = 0,
+        gipChipW = 1,
+        gipChipD = 2,
+        gipPixelW = 3,
+        gipPixelD = 4,
+        gipMaxBinningX = 5,
+        gipMaxBinningY = 6,
+        gipReadModes = 7,
+        gipFilters = 8,
+        gipMinimalExposure = 9,
+        gipMaximalExposure = 10,
+        gipMaximalMoveTime = 11,
+        gipDefaultReadMode = 12,
+        gipPreviewReadMode = 13,
+        gipMaxWindowHeating = 14,
+        gipMaxFan = 15,
+        gipMaxGain = 16,
+        gipMaxPossiblePixelValue = 17,
+        gipLineTime = 18,
+        gipBiasPixelValue = 19,
+        gipFirmwareMajor = 128,
+        gipFirmwareMinor = 129,
+        gipFirmwareBuild = 130,
+        gipDriverMajor = 131,
+        gipDriverMinor = 132,
+        gipDriverBuild = 133,
+        gipFlashMajor = 134,
+        gipFlashMinor = 135,
+        gipFlashBuild = 136
+    }
+
+    public enum MoravianStringParameter : uint {
+        gspCameraDescription = 0,
+        gspManufacturer = 1,
+        gspCameraSerial = 2,
+        gspChipDescription = 3
+    }
+
+    public enum MoravianValueParameter : uint {
+        gvChipTemperature = 0,
+        gvHotTemperature = 1,
+        gvCameraTemperature = 2,
+        gvEnvironmentTemperature = 3,
+        gvSupplyVoltage = 10,
+        gvPowerUtilization = 11,
+        gvADCGain = 20
+    }
+}
---
+++ NINA.Equipment/SDK/CameraSDKs/MoravianSDK/MoravianCameraProvider.cs
@@ -0,0 +1,95 @@
+﻿using NINA.Core.Utility;
+using NINA.Equipment.Equipment.MyCamera;
+using NINA.Equipment.Interfaces;
+using NINA.Equipment.Interfaces.ViewModel;
+using NINA.Image.Interfaces;
+using NINA.Profile.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Text;
+
+namespace MoravianCameraSDK {
+    [Export(typeof(IEquipmentProvider))]
+    public class MoravianCameraProvider : IEquipmentProvider<ICamera> {
+
+        public string Name => "Moravian";
+        public string ContentId => this.GetType().FullName;
+        private readonly IProfileService profileService;
+        private readonly IExposureDataFactory exposureDataFactory;
+
+        [ImportingConstructor]
+        public MoravianCameraProvider(IProfileService profileService, IExposureDataFactory exposureDataFactory) {
+            this.profileService = profileService;
+            this.exposureDataFactory = exposureDataFactory;
+        }
+
+        public IList<ICamera> GetEquipment() {
+            var devices = new List<ICamera>();
+
+            Logger.Debug("Scanning for Moravian Cx USB Cameras");
+            devices.AddRange(GetCameras(() => new MoravianCxUsbSdk(), MoravianCxUsbSdk.Scan()));
+
+            Logger.Debug("Scanning for Moravian Gx USB Cameras");
+            devices.AddRange(GetCameras(() => new MoravianGxUsbSdk(), MoravianGxUsbSdk.Scan()));
+
+            Logger.Debug("Scanning for Moravian Gx Ethernet Cameras");
+            devices.AddRange(GetCameras(() => new MoravianGxEthSdk(), MoravianGxEthSdk.Scan()));
+
+            return devices;
+        }
+
+        private IList<ICamera> GetCameras(Func<IMoravianSDK> sdkFactory, List<uint> cameraIds) {
+            var devices = new List<ICamera>();
+            foreach (var id in cameraIds) {
+                try {
+                    var sdk = sdkFactory();
+                    UIntPtr handle = sdk.Initialize(id);
+                    if (handle == UIntPtr.Zero) {
+                        Logger.Warning($"Moravian SDK: Could not initialize camera id {id}");
+                        continue;
+                    }
+
+                    StringBuilder cameraName = new StringBuilder(256);
+                    if (!sdk.GetStringParameter(handle, MoravianStringParameter.gspCameraDescription, byte.MaxValue, cameraName)) {
+                        Logger.Warning($"Moravian SDK: Could not get camera description for camera id {id}");
+                        continue;
+                    }
+                    StringBuilder cameraSerial = new StringBuilder(256);
+                    if (!sdk.GetStringParameter(handle, MoravianStringParameter.gspCameraSerial, byte.MaxValue, cameraSerial)) {
+                        Logger.Warning($"Moravian SDK: Could not get camera serial for camera id {id}");
+                        continue;
+                    }
+                    
+                    sdk.GetIntegerParameter(handle, MoravianIntegerParameter.gipDriverMajor, out int driverMajor);
+                    sdk.GetIntegerParameter(handle, MoravianIntegerParameter.gipDriverMinor, out int driverMinor);
+                    sdk.GetIntegerParameter(handle, MoravianIntegerParameter.gipDriverBuild, out int driverBuild);
+
+                    sdk.GetIntegerParameter(handle, MoravianIntegerParameter.gipFlashMajor, out int flashMajor);
+                    sdk.GetIntegerParameter(handle, MoravianIntegerParameter.gipFlashMinor, out int flashMinor);
+                    sdk.GetIntegerParameter(handle, MoravianIntegerParameter.gipFlashBuild, out int flashBuild);
+
+                    sdk.GetIntegerParameter(handle, MoravianIntegerParameter.gipFirmwareMajor, out int firmwareMajor);
+                    sdk.GetIntegerParameter(handle, MoravianIntegerParameter.gipFirmwareMinor, out int firmwareMinor);
+                    sdk.GetIntegerParameter(handle, MoravianIntegerParameter.gipFirmwareBuild, out int firmwareBuild);
+
+                    sdk.Release(handle);
+
+                    devices.Add(new MoravianCamera(cameraId: id,
+                                                  id: $"Moravian-{cameraSerial}",
+                                                  name: cameraName.ToString(),
+                                                  category: "Moravian Instruments",
+                                                  driverVersion: $"{driverMajor}.{driverMinor}.{driverBuild}",
+                                                  firmwareVersion: $"{firmwareMajor}.{firmwareMinor}.{firmwareBuild}",
+                                                  flashVersion: $"{flashMajor}.{flashMinor}.{flashBuild}",
+                                                  sdk: sdk,
+                                                  profileService: profileService,
+                                                  exposureDataFactory: exposureDataFactory));
+                } catch (Exception ex) {
+                    Logger.Error($"Moravian SDK: failed to get camera for camera id {id}", ex);
+                }
+            }
+            return devices;
+        }
+    }
+}
