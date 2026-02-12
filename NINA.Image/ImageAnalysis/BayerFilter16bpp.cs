c00c4923d7acfaeae1e74866fcc1265494364eb9
Stefan Berg <stefan.berg@gmx.net>
2026-02-12 16:45:11 +0100

    Update BayerFilter16bpp.cs

--- NINA.Image/ImageAnalysis/BayerFilter16bpp.cs
+++ NINA.Image/ImageAnalysis/BayerFilter16bpp.cs
@@ -130,7 +130,8 @@ namespace NINA.Image.ImageAnalysis {
 
             // Process the interior rows in parallel; the hot path assumes valid neighbors
             // so we peel borders out to a separate pass to avoid per-pixel edge checks.
-            Parallel.For(innerStartY, innerEndY, y => ProcessInnerRow(y, width, srcPtr, dstPtr, srcStride, dstStride, flatPattern));
+            var po = new ParallelOptions { MaxDegreeOfParallelism = Math.Max(1, Environment.ProcessorCount - 1) };
+            Parallel.For(innerStartY, innerEndY, po, y => ProcessInnerRow(y, width, srcPtr, dstPtr, srcStride, dstStride, flatPattern));
 
             ProcessBorders(width, height, srcPtr, dstPtr, srcStride, dstStride);
         }
