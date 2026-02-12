a9b3726ee51ad4a82dae4d4128a164640d7b5eba
Stefan Berg <isbeorn86@googlemail.com>
2026-02-12 18:25:33 +0100

    rename sofa lib to be setup compatible

--- NINA.Astrometry/SOFA.cs
+++ NINA.Astrometry/SOFA.cs
@@ -23,7 +23,7 @@ namespace NINA.Astrometry {
     /// http://www.iausofa.org/current_C.html#Downloads
     /// </summary>
     public static class SOFA {
-        private const string DLLNAME = "SOFA-2023-10-11.dll";
+        private const string DLLNAME = "SOFA_2023_10_11.dll";
 
         static SOFA() {
             DllLoader.LoadDll(Path.Combine("SOFA", DLLNAME));
