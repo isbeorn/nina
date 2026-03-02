bece201890c4b253d96871b3b5c559a283874df9
Marc Blank <marc.blank@live.com>
2026-03-01 19:04:40 -0800

    Make Default for Symbols 0 (was showing up as NaN)

--- NINA.Sequencer/Logic/UserSymbol.cs
+++ NINA.Sequencer/Logic/UserSymbol.cs
@@ -76,6 +76,7 @@ namespace NINA.Sequencer.Logic {
             set {
                 _expr = value;
                 _expr.SymbolBroker = SymbolBroker;
+                _expr.Default = 0;
                 RaisePropertyChanged();
             }
         }
