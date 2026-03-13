b437e95ee533ebd78ad1fd0f81b92bbd5c524465
Marc Blank <marc.blank@live.com>
2026-03-13 15:16:49 -0700

    Clean up

--- NINA.Sequencer/Logic/Expression.cs
+++ NINA.Sequencer/Logic/Expression.cs
@@ -45,6 +45,7 @@ namespace NINA.Sequencer.Logic {
             AutoValue = cloneMe.AutoValue;
             DefaultString = cloneMe.DefaultString;
             Validator = validator;
+            Context = context;
         }
 
         public Expression(string definition, ISequenceEntity context) {
@@ -315,19 +316,7 @@ namespace NINA.Sequencer.Logic {
         public IReadOnlyDictionary<string, UserSymbol> Resolved => resolved.AsReadOnly();
         public string StringValue { get; set; }
         public UserSymbol Symbol { get; set; } = null;
-
-        private ISymbolBroker _symbolBroker;
-        public ISymbolBroker SymbolBroker {
-            get {
-                if (_symbolBroker == null && Context is SequenceEntityINPC entity) {
-                    // Walk up to root container to get SymbolBroker
-                    _symbolBroker = entity.GetSymbolBroker();
-                }
-                return _symbolBroker;
-            }
-            set => _symbolBroker = value;
-        }
-
+        public ISymbolBroker SymbolBroker { get; set; }
         public string Type { get; set; } = "double";
         public Action<Expression> Validator { get; set; }
         public virtual double Value {
