4936a2bc47e96666b31a8ccc67419b65718ad2a0
Marc Blank <marc.blank@live.com>
2026-03-13 15:22:49 -0700

    More...

--- NINA/ViewModel/Sequencer/Sequence2/Sequence2VM.cs
+++ NINA/ViewModel/Sequencer/Sequence2/Sequence2VM.cs
@@ -205,7 +205,6 @@ namespace NINA.ViewModel.Sequencer {
 
                     var rootContainer = SequencerFactory.GetContainer<SequenceRootContainer>();
                     rootContainer.Name = Loc.Instance["LblAdvancedSequenceTitle"];
-                    rootContainer.SymbolBroker = SymbolBroker;
                     rootContainer.Add(SequencerFactory.GetContainer<StartAreaContainer>());
                     rootContainer.Add(SequencerFactory.GetContainer<TargetAreaContainer>());
                     rootContainer.Add(SequencerFactory.GetContainer<EndAreaContainer>());
