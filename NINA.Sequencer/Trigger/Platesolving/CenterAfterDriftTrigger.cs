d2246e04468fd25281e82000d7254a4494d187e5
Stefan Berg <isbeorn86@googlemail.com>
2026-04-19 14:58:44 +0200

    Add a legacy migration corpus to verify importing old sequences into latest

--- NINA.Sequencer/Trigger/Platesolving/CenterAfterDriftTrigger.cs
+++ NINA.Sequencer/Trigger/Platesolving/CenterAfterDriftTrigger.cs
@@ -114,7 +114,7 @@ namespace NINA.Sequencer.Trigger.Platesolving {
         [JsonProperty]
         public InputCoordinates Coordinates { get; set; }
 
-        [IsExpression (Default = 10, Range = [0, 60, ExpressionRange.MIN_EXCLUSIVE], HasValidator = true)]
+        [IsExpression (Default = 10, Range = [0, ExpressionRange.NO_MAXIMUM, ExpressionRange.MIN_EXCLUSIVE], HasValidator = true)]
         public partial double DistanceArcMinutes { get; set; }
 
         partial void DistanceArcMinutesExpressionValidator(Expression expr) {
