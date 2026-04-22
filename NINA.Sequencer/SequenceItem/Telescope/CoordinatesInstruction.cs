670b9df5126aadd230bf818ed1e3304af70c624d
Stefan Berg <isbeorn86@googlemail.com>
2026-04-23 01:45:14 +0200

    clear Issues on  CoordinatesInstruction.Validate

--- NINA.Sequencer/SequenceItem/Telescope/CoordinatesInstruction.cs
+++ NINA.Sequencer/SequenceItem/Telescope/CoordinatesInstruction.cs
@@ -222,8 +222,9 @@ namespace NINA.Sequencer.SequenceItem.Telescope {
         }
 
         public bool Validate() {
+            Issues.Clear();
             Expression.ValidateExpressions(Issues, RaExpression, DecExpression, PositionAngleExpression, OffsetExpression);
-            RaisePropertyChanged("Issues");
+            RaisePropertyChanged(nameof(Issues));
             return Issues.Count == 0;
         }
 
