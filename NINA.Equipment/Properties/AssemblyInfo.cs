6054fb2a11382bebf7a3af3010b1a65159a1fa1c
Stefan Berg <isbeorn86@googlemail.com>
2026-09-09 15:56:01 +0200

    Complete dome parking reliably and enforce its waiting timeout
    Backport the approved topic to the 3.2 release branch.
    
    (cherry picked from commit 6a83379822e0e015d13591a5ede54574ecc3d0e3)

--- NINA.Equipment/Properties/AssemblyInfo.cs
+++ NINA.Equipment/Properties/AssemblyInfo.cs
@@ -27,6 +27,7 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: InternalsVisibleTo("NINA.Test")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("20b7c98e-f7f8-497e-b5e5-02fa99d4b98b")]
